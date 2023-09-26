3c3826bb44ed56fba0721692926d069a91d155e5
Viskus <38486602+Viskus@users.noreply.github.com>
2023-09-26 14:06:23 +1000

    Update HardcoreMode.h

--- src/HardcoreMode.h
+++ src/HardcoreMode.h
@@ -1,12 +1,24 @@
-#pragma once
+#ifndef HARDCOREMODE_H
+#define HARDCOREMODE_H
 
-#include "Player.h" // Include the Player.h header
+#include "Module.h" // Include the Module.h header
 
 class HardcoreModeModule : public Module
 {
 public:
+    HardcoreModeModule();
+    virtual ~HardcoreModeModule();
+
+    // Add your module-specific members and functions here
+
+    // Override necessary Module functions
+    virtual void OnInstall() override;
+    virtual void OnUpdate(uint32 diff) override;
+    virtual void OnConfigLoad(const ConfigEntry* /*entry*/) override;
+
+    // Add the original member functions
     void OnCharacterDeath(Player* player, const char* causeOfDeath);
     void SendDeathMessage(Player* player, const char* causeOfDeath);
 };
 
-extern HardcoreModeModule* g_HardcoreModeModule;
+#endif // HARDCOREMODE_H
